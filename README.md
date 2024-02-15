c8be3ea862696f4c821c42099210ca358f587efe
KaonWry <titan2011official@gmail.com>
2024-02-15 15:31:23 +0700

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# (Somewhat an) Idle Game Made in Python
+
+So one day i was bored and shit and the idea came to me in a dream. Js sucks, web app sucks, so i make it with tkinter as frontend
+
+## How to run
+As i said before this shit uses tkinter as a frontend so if you want to use it you need to install python and tkinter first. then just run the game.py
--- game.py
+++ game.py
@@ -1,29 +1,55 @@
-import os
+import tkinter as tk
 import time
-import keyboard
+import math
 
-def mainLoop():
-    os.system('cls');
-    print (addValue());
-    time.sleep(0.1);
-    
+score = 1
+mult = 0
+
 def addValue():
-    global value;
-    value += 0.1 * multiplier();
-    return round(value, 2);
-
-def multiplier():
-    # global mult;
-    mult = 5;
-    return mult;
-
-value = 0;
-
-while True:
-    mainLoop()
-    try:
-        if keyboard.is_pressed('esc'):
-            exit();
-    except:
-        break;
+    global score
+    score += 0.01 * (1 + mult)
     
+def multiplier():
+    global mult
+    mult += 0.25
+
+def showValue():
+    global score
+    addValue()
+    shownScore = score
+    lblScore['text'] = round(shownScore, 2)
+    window.after(100, showValue)
+    
+def showgGrowth():
+    global mult
+    shownGrowth = str(round(0.01 * (1 + mult), 2))
+    lblGrowth['text'] = f'{shownGrowth}/s'
+    window.after(1, showgGrowth)
+    
+def showMult():
+    global mult
+    shownMult = f'{int(mult*100)}%'
+    lblMult['text'] = shownMult
+    window.after(1, showMult)
+    
+
+window = tk.Tk()
+window.title('CIA Black Project')
+window.minsize(800, 600)
+
+lblScore = tk.Label(window, text='', font=('Helvetica', 30))
+lblScore.grid(row=0, column=0, padx = 10, pady = 10, sticky='w')
+
+lblGrowth = tk.Label(window, text='', font=('Helvetica', 12))
+lblGrowth.grid(row=1, column=0, padx = 10, pady = 0, sticky='w')
+
+btnMult = tk.Button(window, text='Pound me dady UwU', command=multiplier, font=('Helvetica', 9))
+btnMult.grid(row=2, column=0, padx=10, pady=10)
+
+lblMult = tk.Label(window, text='', font=('Helvetica', 12))
+lblMult.grid(row=2, column=2, padx=10, pady=10, sticky='w')
+    
+showValue()
+showMult()
+showgGrowth()
+window.mainloop()
