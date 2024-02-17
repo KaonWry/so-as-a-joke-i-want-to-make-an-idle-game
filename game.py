d661cf2b10ab0b91265ad884b449fa66d5071bab
KaonWry <titan2011official@gmail.com>
2024-02-17 22:39:03 +0700

    made upgrading system for the idle game
    got issues in computing cost, different result in upgradeCost and minCost, will fix later

--- game.py
+++ game.py
@@ -1,73 +1,123 @@
 import tkinter as tk
-import time
 import math
 
-score = 1
-mult = 0
+score = 0
+mult = 1
+upg = [0]
 
-def baseAddValue():
+# Reusable
+def logFunc(a, b, c, upgradeType):
+    global upg
+    return a * math.log(upg[upgradeType - 1], b) + c
+
+def expFunc(a, b, c, upgradeType):
+    global upg
+    return (a * math.pow(upg[upgradeType - 1], b) + c)
+
+def changeState(a):
+    if a['state'] == 'normal':
+        a.config(state='disabled')
+    else:
+        a.config(state='normal')
+        
+# Score
+def addScore():
     global score
-    score += 0.01 * (1 + mult)
-    
-def multiplier():
-    global mult
-    mult += 0.25
+    score += 1 * (1 + mult)
     
 def showValue():
     global score
-    baseAddValue()
-    shownScore = score
-    lblScore['text'] = round(shownScore, 2)
-    window.after(100, showValue)
+    addScore()
+    shownScore.set(score)
+    window.after(1000, showValue)
     
-def showgGrowth():
+def showGrowth():
     global mult
-    shownGrowth = str(round((0.01 * (1 + mult)*10), 3))
-    lblGrowth['text'] = f'{shownGrowth}/s'
-    window.after(1, showgGrowth)
+    shownGrowth.set(f'{0.01 * (mult)*100}/s')
+    window.after(1, showGrowth)
     
-def showMult():
+# Called each button click
+def addMult(upgradeType, upgradeMult):
     global mult
-    shownMult = f'{int(mult*100)}%'
-    lblMult['text'] = shownMult
-    window.after(1, showMult)
+    global upg
+    mult += upgradeMult
+    upg[upgradeType - 1] += 1
+    if upg[upgradeType - 1] == 1:
+        upg.append(0)
+        
+def minScore(a, b, c, upgradeType):
+    global score
+    cost = expFunc(a, b, c, upgradeType)
+    print (f'cost = {cost}')
+    print (f'score before = {score}')
+    score = score - cost
+    print (f'score after = {score}')
+
+def upgButtonClick(upgradeType, upgradeMult, a, b, c):
+    addMult(upgradeType, upgradeMult)
+    minScore(a, b, c, upgradeType)
+
+# Called every ms
+def upgradeCost(upgrVar, a, b, c, upgradeType):
+    show = expFunc(a, b, c, upgradeType)
+    upgrVar.set(show)
     
+def checkUpgradeCost(a, b, c, upgradeType, button):
+    global score
+    cost = expFunc(a, b, c, upgradeType)
+    if score <= cost:
+        button.config(state='disable')
+    else:
+        button.config(state='normal')
+    
+def checkButton(a, b, c, upgradeType, button, upgrVar):
+    upgradeCost(upgrVar, a, b, c, upgradeType)
+    checkUpgradeCost(a, b, c, upgradeType, button)
+    window.after(10, lambda: checkButton(a, b, c, upgradeType, button, upgrVar))
+
+# Debugging
 def boostValue():
     global score
     score += sclAddValue.get()
-    
-def changeState():
-    if btnBoostValue['state'] == 'normal':
-        btnBoostValue.config(state='disabled')
-    else:
-        btnBoostValue.config(state='normal')
 
+# Window Management
 window = tk.Tk()
 window.title('CIA Black Project')
 window.minsize(800, 600)
 
-lblScore = tk.Label(window, text='', font=('Helvetica', 30))
+# Text Display
+shownScore = tk.StringVar()
+shownGrowth = tk.StringVar()
+upgr1Cost = tk.StringVar()
+upgr1Mult = tk.StringVar()
+
+lblScore = tk.Label(window, textvariable=shownScore, font=('Helvetica', 30))
 lblScore.grid(row=0, column=0, columnspan=5, padx = 10, pady = 10, sticky='w')
 
-lblGrowth = tk.Label(window, text='', font=('Helvetica', 12))
-lblGrowth.grid(row=1, column=0, padx = 10, pady = 0, sticky='w')
+lblGrowth = tk.Label(window, textvariable=shownGrowth, font=('Helvetica', 12))
+lblGrowth.grid(row=1, column=0, padx = 10, sticky='w')
 
-btnMult = tk.Button(window, text='Pound me dady UwU', command=multiplier, font=('Helvetica', 9))
-btnMult.grid(row=2, column=0, padx=10, pady=10, sticky='w')
 
-lblMult = tk.Label(window, text='', font=('Helvetica', 12))
-lblMult.grid(row=2, column=1, padx=10, pady=10, sticky='w')
+# Upgrade 1
+btnUpgrade1 = tk.Button(window, textvariable=upgr1Cost, font=('Helvetica', 16), command=lambda: upgButtonClick(1, 1, 1.7, 2.7, 2))
+btnUpgrade1.grid(row=2, column=0, padx= 10, sticky='w')
+checkButton(1.5, 2.5, 2, 1, btnUpgrade1, upgr1Cost)
 
+lblUpgrade1 = tk.Label(window, textvariable=upgr1Mult, font=('Helvetica', 16))
+lblUpgrade1.grid(row=2, column=1, padx= 10, sticky='w')
+
+# Debugging Stuffs
 btnBoostValue = tk.Button(window, text='add value', font=('Helvetica', 12), command=boostValue, state="normal")
-btnBoostValue.grid(row=3, column=0, padx=10, pady=10, sticky='w')
+btnBoostValue.grid(row=99, column=0, padx=10, pady=10, sticky='w')
 
-btnChangeState = tk.Button(window, text='change state', font=('Helvetica',12), command=changeState)
-btnChangeState.grid(row=3, column=1, padx=10, pady=10, sticky='w')
+btnChangeState = tk.Button(window, text='change state', font=('Helvetica',12), command=lambda: changeState(btnBoostValue))
+btnChangeState.grid(row=99, column=1, padx=10, pady=10, sticky='w')
     
 sclAddValue = tk.Scale(window, from_=0, to=1000, orient=tk.HORIZONTAL)
-sclAddValue.grid(row=3, column=2, pady=10)
+sclAddValue.grid(row=99, column=2, pady=10)
 
+
+# Start App    
 showValue()
-showMult()
-showgGrowth()
+showGrowth()
 window.mainloop()
